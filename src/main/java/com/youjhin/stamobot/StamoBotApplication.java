b0ed0be64d85d3823f3a8174e217e0504f5ef9b4
«YouJhin» <palevich86@mail.ru>
2023-11-11 16:02:15 +0300

    Initial commit - Echo Bot

---
+++ src/main/java/com/youjhin/stamobot/StamoBotApplication.java
@@ -0,0 +1,13 @@
+package com.youjhin.stamobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StamoBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StamoBotApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/youjhin/stamobot/bot/StamoBot.java
@@ -0,0 +1,46 @@
+package com.youjhin.stamobot.bot;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class StamoBot extends TelegramLongPollingBot {
+
+    @Value("${bot.name}")
+    private String botName;
+
+    public StamoBot(@Value("${bot.token}") String botToken) {
+        super(botToken);
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        var msg = update.getMessage();
+        var user = msg.getFrom();
+        var id = user.getId();
+
+        sendText(id, msg.getText());
+
+    }
+
+    public void sendText(Long who, String what) {
+        SendMessage sm = SendMessage.builder()
+                .chatId(who.toString()) //Who are we sending a message to
+                .text(what).build();    //Message content
+        try {
+            execute(sm);                        //Actually sending the message
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);      //Any error will be printed here
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botName;
+    }
+}
